066475aa98eec8872faacc9fd5920e10d81b854d
Atsushi Morimoto <74th.tech@gmail.com>
2025-08-03 09:22:40 +0900

    JSONの例を変更

--- 003-edit/go_json.go
+++ 003-edit/go_json.go
@@ -1,3 +1,17 @@
 package main
 
-{"hours": 1, "minutes": 30, "seconds": 0}
+reservation = {
+	"reservation_id": 20134,
+	"reservation_type": "hotel",,
+	"reservation_date": "2023-10-01",
+	"status": "confirmed",
+	"details": {
+		"hotel_name": "Grand Hotel",
+		"room_type": "suite",
+		"check_in": "2023-10-05",
+		"check_out": "2023-10-10"
+	},
+	"user_id": 12345,
+	"created_at": 1,
+	"updated_at": 30
+}
